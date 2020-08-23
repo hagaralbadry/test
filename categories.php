0ff9534cb814b04b3c391319a806f94e5cb9b3c5
hagaralbadry <32043805+hagaralbadry@users.noreply.github.com>
2020-08-23 21:35:20 +0200

    Add files via upload

--- categories.php
+++ categories.php
@@ -24,6 +24,7 @@ table, th, td {
               <tr>
                     <th>ID</th>
                     <th>Category</th>
+                    <th>Sup Category</th>
                     <th>Product name</th>
                     <th>Image</th>
                     <th>Edit </th>
@@ -41,7 +42,8 @@ table, th, td {
                 <td><?php echo $row->id ?></td>
                 <td><?php echo $row->cat_name ?></td>
                 <td><?php echo $row->sup_cat_name ?></td>
-                <td><?php echo $row->sup_cat_name ?></td>
+                <td><?php echo $row->product_name ?></td>
+                <td><?php echo $row->image ?></td>
                 <td><a href='editCategory.php'>Edit Category</a></td>
                 <td><a href='deleteCategory.php'>delete Category</a></td>
             </tbody>
